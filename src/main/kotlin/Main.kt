0768103210be2e3b4f056c26b55604451310c8bb
MaestroMikael <153710935+MaestroMikael@users.noreply.github.com>
2024-01-15 22:02:40 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+
+fun main() {
+
+    fun computeFee(amount: Double) {   //  Задача 1
+        val minTax = 35.0 // минимальная комиссия
+        val taxRate = 0.0075 // комиссия в процентах
+        val tax = maxOf(amount * taxRate, minTax)  // Выбираем вариант комиссии
+        println("Комиссия составит: $tax рублей")
+    }
+
+
+    fun likesName(likes: Int) {  //   Задача 2
+
+        if (likes % 10 != 1 || likes % 100 == 1 || likes == 11) println("Понравилось $likes людям") else println("Понравилось $likes человеку") // Функция определения люди-человеки))
+    }
+
+    fun calculateDiscount(sum: Int, regularUser: Boolean): Double {   //  Задача 3
+        // sum - сумма покупки regularUser - постоянный покупатель
+        var discount = 0.0
+        var result = sum.toDouble()  // приводим в дабл
+        when {
+            sum in 1001..10000 -> discount = 100.0
+            sum > 10000 -> discount = sum * 0.05
+        }
+        result -= discount
+        if (regularUser) {
+            result *= 0.99
+        }
+        return result
+    }
+
+    println("Цена со скидкой = " + calculateDiscount(100000, true))
+
+    likesName(19881)
+
+    computeFee(10000.0)
+}
+
+
